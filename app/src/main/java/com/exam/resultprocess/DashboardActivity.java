d7362b516869f4993eb4e5878fa3327fad64b6af
Sagor Hossain <sagordc2015@gmail.com>
2021-04-18 00:03:33 +0600

    first commit for git

---
+++ app/src/main/java/com/exam/resultprocess/DashboardActivity.java
@@ -0,0 +1,58 @@
+package com.exam.resultprocess;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    Button inputResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+        setSupportActionBar(findViewById(R.id.toolbarId));
+
+        inputResult = (Button) findViewById(R.id.inputResult);
+
+        inputResult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DashboardActivity.this, InputResultActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.profileItem:
+                Toast.makeText(this, "Edit Profile Click", Toast.LENGTH_SHORT).show();
+                Intent intentProfile = new Intent(DashboardActivity.this, EditProfileActivity.class);
+                startActivity(intentProfile);
+                return true;
+            case R.id.logoutItem:
+                Intent intentLogout = new Intent(DashboardActivity.this, MainActivity.class);
+                startActivity(intentLogout);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
---
+++ app/src/main/java/com/exam/resultprocess/MainActivity.java
@@ -0,0 +1,45 @@
+package com.exam.resultprocess;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText username, password;
+    Button login;
+    TextView register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        username = (EditText) findViewById(R.id.username);
+        username.requestFocus();
+        password = (EditText) findViewById(R.id.password);
+        login = (Button) findViewById(R.id.buttonLogin);
+        register = (TextView) findViewById(R.id.register);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DashboardActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
