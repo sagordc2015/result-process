98e7a0ad8106063d26fbb4f051a5dbc37e6efe68
Saffanjaman <saffan.jaman@gmail.com>
2021-04-25 01:52:32 +0600

    Commit for Registration.

--- app/src/main/java/com/exam/resultprocess/RegistrationActivity.java
+++ app/src/main/java/com/exam/resultprocess/RegistrationActivity.java
@@ -7,14 +7,10 @@ import android.widget.EditText;
 
 public class RegistrationActivity extends AppCompatActivity {
 
-    private EditText fullName;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
-        fullName = (EditText) findViewById(R.id.fullName);
     }
-    private EditText email;
 }
